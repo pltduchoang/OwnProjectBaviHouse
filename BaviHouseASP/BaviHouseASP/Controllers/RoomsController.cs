3d90c59e1be812970f532b0417478977de69b3cd
pltduchoang <plt.duchoang@gmail.com>
2023-08-22 09:55:20 -0600

    ASP app

---
+++ BaviHouseASP/BaviHouseASP/Controllers/RoomsController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BaviHouseASP.Data;
+using BaviHouseASP.Models;
+using BaviHouseASP.Data.Migrations;
+
+namespace BaviHouseASP.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoomsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rooms
+        public async Task<IActionResult> Index()
+        {
+              return _context.Room != null ? 
+                          View(await _context.Room.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Room'  is null.");
+        }
+
+        // GET: Rooms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Room == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Room
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // GET: Rooms/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rooms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,RoomNo,FullName,Phone,MoveInDate,Deposit,Rent,WaterLaundry,PreviousePowerReading,PowerReading,PowerConsume,PowerPrice,PowerCost,TotalCharge")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                room.PowerConsume = room.PowerReading - room.PreviousePowerReading;
+                room.PowerCost = room.PowerConsume * room.PowerPrice;
+                room.TotalCharge = room.Rent + room.WaterLaundry + room.PowerCost;
+                _context.Add(room);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(room);
+        }
+
+        // GET: Rooms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Room == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Room.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Rooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,RoomNo,FullName,Phone,MoveInDate,Deposit,Rent,WaterLaundry,PreviousePowerReading,PowerReading,PowerConsume,PowerPrice,PowerCost,TotalCharge")] Room room)
+        {
+            if (id != room.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(room);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomExists(room.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(room);
+        }
+
+        // GET: Rooms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Room == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Room
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // POST: Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Room == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Room'  is null.");
+            }
+            var room = await _context.Room.FindAsync(id);
+            if (room != null)
+            {
+                _context.Room.Remove(room);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomExists(int id)
+        {
+          return (_context.Room?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+
+        public async Task<IActionResult> CalculateRent()
+        {
+            
+            //return _context.Room != null ?
+            //              View(await _context.Room.ToListAsync()) :
+            //              Problem("Entity set 'ApplicationDbContext.Room'  is null.");
+            if (_context.Room is null)
+            {
+                Problem("Entity set 'ApplicationDbContext.Room'  is null.");
+            }
+
+            var RoomList = await _context.Room.ToListAsync();
+            foreach (Room room in RoomList)
+            {
+                room.PreviousePowerReading = room.PowerReading;
+            }
+            return View(RoomList);
+        }
+    }
+}
---
+++ BaviHouseASP/BaviHouseASP/Models/Room.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+namespace BaviHouseASP.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public required int RoomNo { get; set; }
+        public required string FullName { get; set; }
+        public required string Phone { get; set; }
+        public required DateTime MoveInDate { get; set; }
+        public required double Deposit { get; set; }
+        public required double Rent { get; set; }
+        public required double WaterLaundry { get; set;}
+        public int PreviousePowerReading { get; set; }
+        public int PowerReading { get; set; }
+        public double? PowerConsume { get; set; }
+        public double PowerPrice { get; set; }
+        public double? PowerCost { get; set; }
+        public double? TotalCharge { get; set; }
+
+        public Room()
+        {
+            
+        }
+
+    }
+
+}
